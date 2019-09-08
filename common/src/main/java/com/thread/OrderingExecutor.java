54528abd9aec525c8aa0fe62b497b8e8e890d850
fenghaiyu <fhyfhy17@163.com>
2019-09-08 17:16:52 +0800

    线程池

--- common/src/main/java/com/thread/OrderingExecutor.java
+++ common/src/main/java/com/thread/OrderingExecutor.java
@@ -7,16 +7,16 @@ import java.util.Queue;
 import java.util.concurrent.Executor;
 
 /**
-* 有序线程池，可以包装在任何线程池外层，通过对相同的key进行队列执行，效果是，如果
+ * 有序线程池，可以包装在任何线程池外层，通过对相同的key进行队列执行，效果是，如果
  * * 线程池中无相同key的待执行任务，该任务做为普通线程池任务，可以在任何一条线程里执行，
  * 如果池程池中有相同key的待执行任务，该任务加到待执行任务的队列里连续执行
-*/
-public class OrderingExecutor implements Executor{
+ */
+public class OrderingExecutor implements Executor {
 
     private final Executor delegate;
     private final Map<Object, Queue<Runnable>> keyedTasks = new HashMap<>();
 
-    public OrderingExecutor(Executor delegate){
+    public OrderingExecutor(Executor delegate) {
         this.delegate = delegate;
     }
 
@@ -26,17 +26,17 @@ public class OrderingExecutor implements Executor{
     }
 
     public void execute(Runnable task, Object key) {
-        if (key == null){
+        if (key == null) {
             execute(task);
             return;
         }
 
         boolean first;
         Runnable wrappedTask;
-        synchronized (keyedTasks){
+        synchronized (keyedTasks) {
             Queue<Runnable> dependencyQueue = keyedTasks.get(key);
             first = (dependencyQueue == null);
-            if (dependencyQueue == null){
+            if (dependencyQueue == null) {
                 dependencyQueue = new LinkedList<>();
                 keyedTasks.put(key, dependencyQueue);
             }
@@ -55,7 +55,7 @@ public class OrderingExecutor implements Executor{
         return new OrderedTask(task, dependencyQueue, key);
     }
 
-    class OrderedTask implements Runnable{
+    class OrderedTask implements Runnable {
 
         private final Queue<Runnable> dependencyQueue;
         private final Runnable task;
@@ -69,18 +69,18 @@ public class OrderingExecutor implements Executor{
 
         @Override
         public void run() {
-            try{
+            try {
                 task.run();
             } finally {
                 Runnable nextTask = null;
-                synchronized (keyedTasks){
-                    if (dependencyQueue.isEmpty()){
+                synchronized (keyedTasks) {
+                    if (dependencyQueue.isEmpty()) {
                         keyedTasks.remove(key);
-                    }else{
+                    } else {
                         nextTask = dependencyQueue.poll();
                     }
                 }
-                if (nextTask!=null)
+                if (nextTask != null)
                     delegate.execute(nextTask);
             }
         }
