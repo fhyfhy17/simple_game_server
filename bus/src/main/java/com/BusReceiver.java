5576e291ed3e4859664838341ce4503bc3c3e6e0
fenghaiyu <fhyfhy17@163.com>
2019-09-16 22:57:18 +0800

    ~~~~~~

--- bus/src/main/java/com/BusReceiver.java
+++ bus/src/main/java/com/BusReceiver.java
@@ -19,23 +19,21 @@ public class BusReceiver extends BaseReceiver {
 
     @PostConstruct
     public void startup() {
-        m = new MessageGroup(TypeEnum.GroupEnum.BUS_GROUP.name()) {
+        m = new MessageGroup(TypeEnum.GroupEnum.BUS_GROUP.name(), 1) {
             @Override
             public MessageThreadHandler getMessageThreadHandler() {
                 return new BusMessageHandler();
             }
+
             @Override
             public void messageReceived(Packet msg) {
-        
+
                 // 分配执行器执行
-                //TODO ID的分配策略  包括服与服之间
-                int index = Math.abs(msg.getFrom().hashCode()) % handlerCount;
-                
-                MessageThreadHandler handler = handlerList.get(index);
+                MessageThreadHandler handler = handlerList.get(0);
                 handler.messageReceived(msg);
             }
-            
-    
+
+
         };
         m.startup();
 
