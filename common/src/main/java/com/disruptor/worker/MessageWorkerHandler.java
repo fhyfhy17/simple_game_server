99aa7240ccbf570d5fe42546362137fbfc673bf5
fenghaiyu <fenghaiyu@fotoable.com>
2019-09-18 11:28:31 +0800

    ~~~~~~~

--- common/src/main/java/com/disruptor/worker/MessageWorkerHandler.java
+++ common/src/main/java/com/disruptor/worker/MessageWorkerHandler.java
@@ -16,26 +16,26 @@ public class MessageWorkerHandler extends WorkerHandler {
 
     @Override
     public void onEvent(DisruptorEvent event) throws Exception {
-        MessageEvent messageEvent = (MessageEvent) event.getBaseEvent();
-        Packet message = messageEvent.getMessage();
-        try {
-            final int cmdId = message.getId();
-
-            ControllerHandler handler = ControllerFactory.getControllerMap().get(cmdId);
-            if (handler == null) {
-                throw new IllegalStateException("收到不存在的消息，消息ID=" + cmdId);
-            }
-            //拦截器前
-            if (!HandlerExecutionChain.applyPreHandle(message, handler)) {
-                return;
-            }
-            //针对method的每个参数进行处理， 处理多参数,返回result
-            com.google.protobuf.Message result = (com.google.protobuf.Message) handler.invokeForController(message);
-            //拦截器后
-//            HandlerExecutionChain.applyPostHandle(message, result, handler);
-        } catch (Exception e) {
-            log.error("", e);
-        }
+//        MessageEvent messageEvent = (MessageEvent) event.getBaseEvent();
+//        Packet message = messageEvent.getMessage();
+//        try {
+//            final int cmdId = message.getId();
+//
+//            ControllerHandler handler = ControllerFactory.getControllerMap().get(cmdId);
+//            if (handler == null) {
+//                throw new IllegalStateException("收到不存在的消息，消息ID=" + cmdId);
+//            }
+//            //拦截器前
+//            if (!HandlerExecutionChain.applyPreHandle(message, handler,m)) {
+//                return;
+//            }
+//            //针对method的每个参数进行处理， 处理多参数,返回result
+//            com.google.protobuf.Message result = (com.google.protobuf.Message) handler.invokeForController(message);
+//            //拦截器后
+////            HandlerExecutionChain.applyPostHandle(message, result, handler);
+//        } catch (Exception e) {
+//            log.error("", e);
+//        }
 
 
     }
