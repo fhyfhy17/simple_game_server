8c6de4df6c140b908505b48aa2b99f86c5961109
fenghaiyu <fenghaiyu@fotoable.com>
2019-10-23 15:15:01 +0800

    ~

--- battle/src/main/java/com/service/map/core/astar/AStar.java
+++ battle/src/main/java/com/service/map/core/astar/AStar.java
@@ -59,45 +59,25 @@ public class AStar {
      * @return 寻路列表
      */
     private LinkedList<Node> search(Node startNode, Node targetNode) {
-
-        // 设定起始节点参数
         startNode.setCostFromStart(0);
         startNode.setCostToTarget(startNode.getCost(targetNode));
         startNode.parentNode = null;
-        // 加入运算等级序列
         openList.offer(startNode);
-        // 当运算等级序列中存在数据时，循环处理寻径，直到levelList为空
         while (!openList.isEmpty()) {
-            // 取出并删除最初的元素
             Node firstNode = openList.poll();
-            // 判定是否和目标node坐标相等
             if (firstNode.equals(targetNode)) {
-                // 是的话即可构建出整个行走路线图，运算完毕
                 return makePath(firstNode);
             } else {
-                // 否则加入已验证List
                 closedList.add(firstNode);
-                // 获得firstNode的移动区域
                 List<Node> _limit = grid.getNearByNodes(firstNode.getX(), firstNode.getY());
-                // 遍历
-                for (int i = 0; i < _limit.size(); i++) {
-                    // 获得相邻节点
-                    Node neighborNode = _limit.get(i);
-                    // 获得是否满足等级条件
-                    boolean isOpen = openList.contains(neighborNode);
-                    // 获得是否已行走
-                    boolean isClosed = closedList.contains(neighborNode);
-                    // 判断是否无法通行
-                    boolean canMove = canMove(neighborNode.getX(), neighborNode.getY());
-                    // 当三者判定皆非时
-                    if (!isOpen && !isClosed && canMove) {
-                        // 设定costFromStart
+                for(Node neighborNode : _limit){
+                    boolean isOpen=openList.contains(neighborNode);
+                    boolean isClosed=closedList.contains(neighborNode);
+                    boolean canMove=canMove(neighborNode.getX(),neighborNode.getY());
+                    if(!isOpen && !isClosed && canMove){
                         neighborNode.setCostFromStart(firstNode.getCostFromStart() + 1);
-                        // 设定costToObject
                         neighborNode.setCostToTarget(neighborNode.getCost(targetNode));
-                        // 改变neighborNode父节点
-                        neighborNode.parentNode = firstNode;
-                        // 加入
+                        neighborNode.parentNode=firstNode;
                         openList.offer(neighborNode);
                     }
                 }
@@ -105,7 +85,6 @@ public class AStar {
 
         }
         closedList.clear();
-        // 当while无法运行时，将返回null
         return null;
     }
 
@@ -138,14 +117,10 @@ public class AStar {
      */
     private LinkedList<Node> makePath(Node node) {
         LinkedList<Node> path = new LinkedList<>();
-        // 当上级节点存在时
         while (node.parentNode != null) {
-            // 在第一个元素处添加
             path.addFirst(node);
-            // 将node赋值为parent node
             node = node.parentNode;
         }
-        // 在第一个元素处添加
         path.addFirst(node);
         return path;
     }
